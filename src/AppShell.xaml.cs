2346ae8314753d42147a21e353ee3da107b90f28
Enrique Largacha Gil <enrique.largacha@outlook.com>
2026-02-21 22:38:30 +0100

    Add MAUI project

---
+++ src/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI.CleanArchitectureTemplate
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
